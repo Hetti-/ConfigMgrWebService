e74ca605efad66a808c3966f0e46a0b7b8064304
Bernd <bernd.hetlinger@gmail.com>
2018-09-24 12:00:05 +0200

    Added Class for CMDevice

---
+++ ConfigMgrWebService/CMDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConfigMgrWebService
+{
+    public class CMDevice
+    {
+        public string DeviceName { get; set; }
+        public string ResourceID { get; set; }
+        public string SMBIOSGUID { get; set; }
+        public string[] MACAddresses { get; set; }
+        public string[] SystemOUName { get; set; }
+        public string LastLogonUserName { get; set; }
+    }
+}
